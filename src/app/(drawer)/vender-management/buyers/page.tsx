701a2331c52d12c4aee2b5743b418501648200e0
Sunny singh <sunnysingh.2044@hotmail.com>
2024-11-17 21:44:12 +0530

    Create Separate Vendor Management Buyer

--- src/app/(drawer)/vender-management/buyers/page.tsx
+++ src/app/(drawer)/vender-management/buyers/page.tsx
@@ -1,39 +1,40 @@
 // vendor-management/buyers/page.tsx
 "use client";
 
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import AddNewBuyerDialog from '@/app/components/AddNewBuyerDialog';
-import PrimaryTable from '@/app/components/PrimaryTable';
-import { Data } from "@/types/types"; 
-import { useSelector, useDispatch } from 'react-redux';
-import { addBuyer } from '@/redux/slices/buyerSlice';
-import { RootState, AppDispatch } from '@/redux/store';
-import { v4 as uuidv4 } from 'uuid'; // Import uuid for unique IDs
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import AddNewBuyerDialog from "@/app/components/AddNewBuyerDialog";
+import PrimaryTable from "@/app/components/PrimaryTable";
+import { Data } from "@/types/types";
+import { useSelector, useDispatch } from "react-redux";
+import { addBuyer } from "@/redux/slices/buyerSlice";
+import { RootState, AppDispatch } from "@/redux/store";
+import { v4 as uuidv4 } from "uuid"; // Import uuid for unique IDs
+import VendorManagementBuyers from "@/app/components/VendorManagementBuyers";
 
 const Buyers = () => {
-    const buyers = useSelector((state: RootState) => state.buyer.buyers);
-    const dispatch = useDispatch<AppDispatch>();
+  const buyers = useSelector((state: RootState) => state.buyer.buyers);
+  const dispatch = useDispatch<AppDispatch>();
 
-    // Function to handle adding a new buyer with unique ID
-    const addNewBuyer = (buyer: Omit<Data, 'id'>) => {
-        const newBuyer = { ...buyer, id: uuidv4() }; // Add unique ID
-        dispatch(addBuyer(newBuyer));
-    };
+  // Function to handle adding a new buyer with unique ID
+  const addNewBuyer = (buyer: Omit<Data, "id">) => {
+    const newBuyer = { ...buyer, id: uuidv4() }; // Add unique ID
+    dispatch(addBuyer(newBuyer));
+  };
 
-    return (
-        <Box className="bg-white border border-[#E8EBED] rounded-xl p-6 h-[calc(100vh-116px)] overflow-auto">
-            <Box className="w-full flex items-center justify-between">
-                <Typography className="text-2xl font-bold">
-                    Vendor Management / Buyers
-                </Typography>
-                <AddNewBuyerDialog onAddBuyer={addNewBuyer} />
-            </Box>
-            <Box className="mt-4">
-                <PrimaryTable rows={buyers} />
-            </Box>
-        </Box>
-    );
+  return (
+    <Box className="bg-white border border-[#E8EBED] rounded-xl p-6 h-[calc(100vh-116px)] overflow-auto">
+      <Box className="w-full flex items-center justify-between">
+        <Typography className="text-2xl font-bold">
+          Vendor Management / Buyers
+        </Typography>
+        <AddNewBuyerDialog onAddBuyer={addNewBuyer} />
+      </Box>
+      <Box className="mt-4">
+        <VendorManagementBuyers />
+      </Box>
+    </Box>
+  );
 };
 
 export default Buyers;
